62a272676bb2bae79416d70a0c58a577e194f05a
JoseR <43625732+nekronrt@users.noreply.github.com>
2019-08-16 14:40:11 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 pwd
+rm -r jenkins-build
 git clone https://github.com/nekronrt/jenkins-build.git
 cd jenkins-build
-git pull
 echo deploying image
 docker rm -f jenkins || true
 docker-compose up -d
